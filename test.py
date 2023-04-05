a4bc09bc2a387584c24fcaeae3191c19fd8e996c
Kangbeen Ko <sweetrainforyou@gmail.com>
2023-04-05 22:38:13 +0900

    feat(test.py): minimal functionalities

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+import os
+import numpy as np
 from nsfw_detector import predict
+
 model = predict.load_model('./pretrained/nsfw_mobilenet2.224x224.h5')
 
+directory_path = "./images/"
+
 # Predict for all images in a directory
-predict.classify(model, './images')
+for root, dirs, files in os.walk(directory_path):
+    for filename in files:
+        file_path = os.path.join(root, filename)
+        result_dic = predict.classify(model, file_path)[file_path]
+        result_key = max(result_dic, key=result_dic.get)
+
+        print(f'{file_path.replace(directory_path, "")}: {result_key} {(result_dic[result_key])*100}%')
